5c71a77e8f63aa0dd92618c65b4ebfeefb00fb0b
Atemu <atemu.main@gmail.com>
2020-03-07 19:37:06 +0100

    Fetch auth file

--- bw-dl.bash
+++ bw-dl.bash
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 cid="$1"
+
+# This is a JSON which gives us three values that are all required to download
+# the images. Requesting it needs a Browser ID which is never checked against
+# anything, so for our purposes, it's 0.
+auth="$(curl "https://viewer-trial.bookwalker.jp/trial-page/c?cid=$cid&BID=0")"
