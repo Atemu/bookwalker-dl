0df0b1eece9b83bede705d3d20c87f8efda5c79f
Atemu <atemu.main@gmail.com>
2020-03-08 15:04:13 +0100

    Don't use a temporary file to hold the metadata
    Fixes #5

--- bw-dl.bash
+++ bw-dl.bash
@@ -15,15 +15,15 @@ keyPairId="$(echo "$auth" | jq -r .auth_info.\"Key-Pair-Id\")"
 authString='?pfCd='$pfcd'&Policy='$policy'&Signature='$signature'&Key-Pair-Id='$keyPairId
 
 # Download the book's metadata
-curl "https://viewer-epubs-trial.bookwalker.jp/special/bw/$cid/SVGA/normal_default/configuration_pack.json$authString" > configuration_pack.json
+metadata="$(curl "https://viewer-epubs-trial.bookwalker.jp/special/bw/$cid/SVGA/normal_default/configuration_pack.json$authString")"
 
 # configuration.contents is an array that contains the chapters with metadata in order
-numChapters="$(jq '.configuration.contents | length' configuration_pack.json)"
+numChapters="$(echo $metadata | jq '.configuration.contents | length')"
 
 for chapter in `seq 0 $[numChapters - 1]` ; do
     # Chapter metadata is indexed by its path
-    keyName="$(jq -r ".configuration.contents[$chapter].file" configuration_pack.json)"
-    numPages=$(jq -r .\"$keyName\".FileLinkInfo.PageCount configuration_pack.json)
+    keyName="$(echo $metadata | jq -r ".configuration.contents[$chapter].file")"
+    numPages=$(echo $metadata | jq -r .\"$keyName\".FileLinkInfo.PageCount)
 
     for page in `seq 0 $[numPages - 1]` ; do
         # The keyName is the patch, we can simply put it in the URL
