3b148e56b6c819f2f5c6fccb13d2b066b41fcc0b
Atemu <atemu.main@gmail.com>
2020-03-08 03:06:34 +0100

    Print the URL to every page

--- bw-dl.bash
+++ bw-dl.bash
@@ -24,4 +24,9 @@ for chapter in `seq 0 $[numChapters - 1]` ; do
     # Chapter metadata is indexed by its path
     keyName="$(jq -r ".configuration.contents[$chapter].file" configuration_pack.json)"
     numPages=$(jq -r .\"$keyName\".FileLinkInfo.PageCount configuration_pack.json)
+
+    for page in `seq 0 $[numPages - 1]` ; do
+        # The keyName is the patch, we can simply put it in the URL
+        echo "https://viewer-epubs-trial.bookwalker.jp/special/bw/$cid/SVGA/normal_default/$keyName/$page.jpeg$authString"
+    done
 done
