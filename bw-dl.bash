6bcc8b35cf5198dbd9905b3c848f24b8f3993d92
Atemu <atemu.main@gmail.com>
2020-03-10 17:59:31 +0100

    Create a download directory
    It can be configured via the second argument for now

--- bw-dl.bash
+++ bw-dl.bash
@@ -2,6 +2,10 @@
 
 cid="$1"
 
+if [ "$2" != "" ] ; then
+    downloadDir="$2"/
+fi
+
 # This is a JSON which gives us three values that are all required to download
 # the images. Requesting it needs a Browser ID which is never checked against
 # anything, so for our purposes, it's 0.
@@ -13,6 +17,10 @@ signature="$(echo "$auth" | jq -r .auth_info.Signature)"
 keyPairId="$(echo "$auth" | jq -r .auth_info.\"Key-Pair-Id\")"
 
 authString='?pfCd='$pfcd'&Policy='$policy'&Signature='$signature'&Key-Pair-Id='$keyPairId
+
+# If the variable is empty, it'll download to ./
+bookPath="$downloadDir./$cid"
+mkdir -p "$bookPath"
 
 # Download the book's metadata
 metadata="$(curl "https://viewer-epubs-trial.bookwalker.jp/special/bw/$cid/SVGA/normal_default/configuration_pack.json$authString")"
